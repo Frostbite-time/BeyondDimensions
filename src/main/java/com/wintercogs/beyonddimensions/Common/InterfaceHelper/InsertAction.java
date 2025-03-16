dd9b023a0a418f88a5b12bf54863b4e36377b649
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-03-16 10:17:51 +0800

    删除添加文件，回滚到泛化之前的版本

--- src/main/java/com/wintercogs/beyonddimensions/Common/InterfaceHelper/InsertAction.java
+++
@@ -1,7 +0,0 @@
-package com.wintercogs.beyonddimensions.Common.InterfaceHelper;
-
-@FunctionalInterface
-public interface InsertAction<T>
-{
-    long insert(T handler, int slot, Object stack, boolean simulate);
-}
--- src/main/java/com/wintercogs/beyonddimensions/Common/InterfaceHelper/SlotsCount.java
+++
@@ -1,7 +0,0 @@
-package com.wintercogs.beyonddimensions.Common.InterfaceHelper;
-
-@FunctionalInterface
-public interface SlotsCount<T>
-{
-    int getSlots(T handler);
-}
