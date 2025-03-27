a6812b50abdfee81ff8d1fa4df80813c37810a2f
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-03-27 17:03:00 +0800

    尝试性的重建了渲染类

--- src/main/java/com/wintercogs/beyonddimensions/Render/IngredientRenderer.java
+++ src/main/java/com/wintercogs/beyonddimensions/Render/IngredientRenderer.java
@@ -1,31 +1,29 @@
 package com.wintercogs.beyonddimensions.Render;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.GameRenderer;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import org.joml.Matrix4f;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
+
 
 // 参考自jei的IIngredientRenderer接口以及FluidTankRenderer类
-@OnlyIn(Dist.CLIENT)
-public class IngredientRenderer
-{
+@SideOnly(Side.CLIENT)
+public class IngredientRenderer {
     private static final int TEXTURE_SIZE = 16;
-    private static final int MIN_FLUID_HEIGHT = 1; // ensure tiny amounts of fluid are still visible
+    private static final int MIN_FLUID_HEIGHT = 1;
 
-    public static void drawTiledSprite(GuiGraphics guiGraphics, final int tiledWidth, final int tiledHeight, int color, long scaledAmount, TextureAtlasSprite sprite, int posX, int posY) {
+    public static void drawTiledSprite(int tiledWidth, int tiledHeight, int color, long scaledAmount, TextureAtlasSprite sprite, int posX, int posY) {
+        GlStateManager.enableBlend();
+        GlStateManager.color(1, 1, 1, 1);
 
-        RenderSystem.enableBlend();
-
-
-        RenderSystem.setShaderTexture(0, sprite.atlasLocation());
-        Matrix4f matrix = guiGraphics.pose().last().pose();
+        Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
         setGLColorFromInt(color);
 
         final int xTileCount = tiledWidth / TEXTURE_SIZE;
@@ -45,43 +43,38 @@ public class IngredientRenderer
                     long maskTop = TEXTURE_SIZE - height;
                     int maskRight = TEXTURE_SIZE - width;
 
-                    drawTextureWithMasking(matrix, x, y, sprite, maskTop, maskRight, 100);
+                    drawTextureWithMasking(x, y, sprite, maskTop, maskRight);
                 }
             }
         }
 
-        RenderSystem.setShaderColor(1, 1, 1, 1);
-        RenderSystem.disableBlend();
-
+        GlStateManager.color(1, 1, 1, 1);
+        GlStateManager.disableBlend();
     }
 
     private static void setGLColorFromInt(int color) {
         float red = ((color >> 16) & 255) / 256f;
         float green = ((color >> 8) & 255) / 256f;
         float blue = (color & 255) / 256f;
-        //float alpha = ((color >> 24) & 0xFF) / 255F;
-        float alpha = 1;
-
-        RenderSystem.setShaderColor(red, green, blue, alpha);
+        GlStateManager.color(red, green, blue, 1.0F);
     }
 
-    private static void drawTextureWithMasking(Matrix4f matrix, float xCoord, float yCoord, TextureAtlasSprite textureSprite, long maskTop, long maskRight, float zLevel) {
-        float uMin = textureSprite.getU0();
-        float uMax = textureSprite.getU1();
-        float vMin = textureSprite.getV0();
-        float vMax = textureSprite.getV1();
+    private static void drawTextureWithMasking(float xCoord, float yCoord, TextureAtlasSprite textureSprite, long maskTop, long maskRight) {
+        float uMin = textureSprite.getMinU();
+        float uMax = textureSprite.getMaxU();
+        float vMin = textureSprite.getMinV();
+        float vMax = textureSprite.getMaxV();
+
         uMax = uMax - (maskRight / 16F * (uMax - uMin));
         vMax = vMax - (maskTop / 16F * (vMax - vMin));
 
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-
-        Tesselator tessellator = Tesselator.getInstance();
-        BufferBuilder bufferBuilder = tessellator.getBuilder();
-        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-        bufferBuilder.vertex(matrix, xCoord, yCoord + 16, zLevel).uv(uMin, vMax).endVertex();
-        bufferBuilder.vertex(matrix, xCoord + 16 - maskRight, yCoord + 16, zLevel).uv(uMax, vMax).endVertex();
-        bufferBuilder.vertex(matrix, xCoord + 16 - maskRight, yCoord + maskTop, zLevel).uv(uMax, vMin).endVertex();
-        bufferBuilder.vertex(matrix, xCoord, yCoord + maskTop, zLevel).uv(uMin, vMin).endVertex();
-        tessellator.end();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
+        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+        buffer.pos(xCoord, yCoord + 16, 100).tex(uMin, vMax).endVertex();
+        buffer.pos(xCoord + 16 - maskRight, yCoord + 16, 100).tex(uMax, vMax).endVertex();
+        buffer.pos(xCoord + 16 - maskRight, yCoord + maskTop, 100).tex(uMax, vMin).endVertex();
+        buffer.pos(xCoord, yCoord + maskTop, 100).tex(uMin, vMin).endVertex();
+        tessellator.draw();
     }
 }
