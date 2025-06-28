26435617b756fe1a4da3b66d7b21b8b33db206d9
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-06-28 12:22:15 +0800

    删除多余代码

--- src/main/java/com/wintercogs/beyonddimensions/Menu/BDDisorderedContainerMenu.java
+++ src/main/java/com/wintercogs/beyonddimensions/Menu/BDDisorderedContainerMenu.java
@@ -119,14 +119,7 @@ public abstract class BDDisorderedContainerMenu extends BDBaseMenu
                     slot.tryRemove(cacheStack.getCount() - remaining,Integer.MAX_VALUE,player);
                 }
             }
-            if (cacheStack.isEmpty()) {
-                // 对于维度网络通过玩家设置一个EMPTY无影响
-                // 对于背包槽位可以用于清空当前槽位物品
-                // 对于双方，都可以设置脏数据请求保存
-                slot.setByPlayer(ItemStack.EMPTY);
-            } else {
-                slot.setChanged();
-            }
+            slot.setChanged();
         }
         return ItemStack.EMPTY;
     }
