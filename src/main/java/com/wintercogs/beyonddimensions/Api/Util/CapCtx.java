50b3aa4706cf4e046fb83c73ea8f10d42d61f417
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-08-14 01:14:16 +0800

    添加可空声明

--- src/main/java/com/wintercogs/beyonddimensions/Api/Util/CapCtx.java
+++ src/main/java/com/wintercogs/beyonddimensions/Api/Util/CapCtx.java
@@ -4,7 +4,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.Nullable;
 
-public record CapCtx(Level level, BlockPos pos, Direction direction, BlockEntity be)
+public record CapCtx(Level level, BlockPos pos, @Nullable Direction direction, BlockEntity be)
 {
 }
