70dfb27e7633f877ab1d3cdf696641eac4faa7fa
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-08-14 14:48:31 +0800

    优化网络接口工作逻辑

--- src/main/java/com/wintercogs/beyonddimensions/BlockEntity/Custom/NetInterfaceBlockEntity.java
+++ src/main/java/com/wintercogs/beyonddimensions/BlockEntity/Custom/NetInterfaceBlockEntity.java
@@ -97,6 +97,12 @@ public class NetInterfaceBlockEntity extends BaseMachineBlockEntity implements M
     @Override
     public boolean shouldWork()
     {
+        // 无论接口是否工作，始终保持与网络的内容更新
+        if(getNet() != null)
+        {
+            transferToNet();
+            transferFromNet();
+        }
         return super.shouldWork(); // 接口方块使用内部缓存进行弹出，因此不需要检测getNet
     }
 
@@ -104,11 +110,6 @@ public class NetInterfaceBlockEntity extends BaseMachineBlockEntity implements M
     public void workContent()
     {
         super.workContent();
-        if(getNet() != null)
-        {
-            transferToNet();
-            transferFromNet();
-        }
         // 尝试输出物品到周围
         if(popMode == PopMode.OPEN)
         {
@@ -227,8 +228,10 @@ public class NetInterfaceBlockEntity extends BaseMachineBlockEntity implements M
                 IStackType stack = stackHandler.getStackBySlot(i);
                 if(stack !=null &&!stack.isEmpty())
                 {
-                    net.getUnifiedStorage().insert(stack.copy(),false);
-                    stackHandler.setStackDirectly(i, new ItemStackType());
+                    IStackType extracted = stackHandler.extract(i, stack.getStackAmount(),false);
+                    IStackType remaining = net.getUnifiedStorage().insert(extracted,false);
+                    if(!remaining.isEmpty())
+                        stackHandler.insert(i, remaining, false);
                 }
             }
         }
@@ -250,7 +253,7 @@ public class NetInterfaceBlockEntity extends BaseMachineBlockEntity implements M
                 if(flag!=null && !flag.isEmpty())
                 {
                     // 到达数量上限或者是不同物品则不尝试插入
-                    IStackType current = stackHandler.getStorage().get(i);
+                    IStackType current = stackHandler.getStackBySlot(i);
                     if(current != null &&!current.isEmpty())
                     {
                         if(current.getVanillaMaxStackSize() >= current.getStackAmount())
@@ -268,7 +271,7 @@ public class NetInterfaceBlockEntity extends BaseMachineBlockEntity implements M
                     if(stack !=null &&!stack.isEmpty())
                     {
                         IStackType remaining = stackHandler.insert(i,stack.copy(),false);
-                        if(remaining.getStackAmount()<stack.getStackAmount())
+                        if(!remaining.isEmpty())
                         {
                             net.getUnifiedStorage().insert(remaining.copy(),false);
                         }
