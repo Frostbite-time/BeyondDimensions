bc0d0ac1fb0c3e301501e850ea3d19b170d25a6a
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-08-15 18:35:12 +0800

    完成了对AE磁盘缓存优化

--- src/main/java/com/wintercogs/beyonddimensions/Integration/AE/BD_AEPlugin.java
+++ src/main/java/com/wintercogs/beyonddimensions/Integration/AE/BD_AEPlugin.java
@@ -6,6 +6,8 @@ public class BD_AEPlugin
 {
     public static void register()
     {
+        // AE的网络仅会在需要时从元件中获取全部信息
+        // 配合元件内部的缓存机制，几乎完全抹平了和原生元件的性能差距
         StorageCells.addCellHandler(CellHandler.INSTANCE);
     }
 }
