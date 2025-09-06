9356cad5bbab55b4855f66cc3d4bfd2ac2291968
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-09-06 15:25:23 +0800

    添加新定义，并完成基本的接口

---
+++ src/main/java/com/wintercogs/beyonddimensions/Api/DataBase/Stack/KeyAmount.java
@@ -0,0 +1,14 @@
+package com.wintercogs.beyonddimensions.Api.DataBase.Stack;
+
+/**
+ * 一个包含key和amount的记录类
+ * <p>
+ * 一般仅作于外部的只读视图
+ */
+public record KeyAmount(IStackKey<?> key, long amount)
+{
+    public boolean isEmpty()
+    {
+        return key == null || amount <= 0L;
+    }
+}
