a360a3ef5cd39d6f01cef8aebf4ba59378c56c47
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2025-10-25 12:09:58 +0800

    修复奶桶无限问题

--- src/main/java/com/wintercogs/beyonddimensions/Menu/Slot/OrderedStackTypedSlot.java
+++ src/main/java/com/wintercogs/beyonddimensions/Menu/Slot/OrderedStackTypedSlot.java
@@ -20,10 +20,7 @@ import com.wintercogs.beyonddimensions.Unit.XpUtil;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
@@ -92,7 +89,7 @@ public class OrderedStackTypedSlot extends AbstractStackTypedSlot
                 // 堆叠数量为1 右键点击 尝试取出内容物并插入
                 else if(carriedItem.getCount()==1 && button== GLFW.GLFW_MOUSE_BUTTON_RIGHT && !ItemCapInteractionBlackList.isInBlackList(carriedItem.getItem()))
                 {
-                    if(carriedItem.getItem() instanceof BucketItem bucketItem)
+                    if(carriedItem.getItem() instanceof BucketItem bucketItem || carriedItem.getItem() instanceof MilkBucketItem)
                     {
                         LazyOptional<IFluidHandlerItem> handler = carriedItem.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
                         if(handler.isPresent())
@@ -289,12 +286,12 @@ public class OrderedStackTypedSlot extends AbstractStackTypedSlot
                     // 最后检查是否为能力系统交互
                     else if(carriedItem.getCount() == 1 && button == GLFW.GLFW_MOUSE_BUTTON_RIGHT && !ItemCapInteractionBlackList.isInBlackList(carriedItem.getItem()))
                     {
-                        if(carriedItem.getItem() instanceof BucketItem bucket)
+                        if(carriedItem.getItem() instanceof BucketItem || carriedItem.getItem() instanceof MilkBucketItem)
                         {
                             // 需要分开处理，分别处理
                             // 1.空桶接受
                             // 2.桶向原有区域继续投放
-                            if(bucket == Items.BUCKET) // 空桶接受
+                            if(carriedItem.getItem() == Items.BUCKET) // 空桶接受
                             {
                                 if(clickStack instanceof FluidStackType fluidStackType)
                                 {
