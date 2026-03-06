924596454e0e96c5929d75aff9b178391ed6f6e2
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-03-07 00:21:15 +0800

    递进版本号

--- src/main/java/com/wintercogs/beyonddimensions/api/ids/BDBlockIds.java
+++ src/main/java/com/wintercogs/beyonddimensions/api/ids/BDBlockIds.java
@@ -18,7 +18,7 @@ public class BDBlockIds
 
     // 模块方块
     public static final String ARS_SOURCE_PATHWAY = "ars_source_pathway";
-    public static final String RS_NET_PATHWAY = "rs_net_pathway";
     public static final String MANA_POOL_PATHWAY = "mana_pool_pathway";
     public static final String SCHEMATICANNON_PATHWAY = "schematicannon_pathway";
+    public static final String RS_NET_PATHWAY = "rs_net_pathway";
 }
