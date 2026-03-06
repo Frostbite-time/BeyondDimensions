5b30970cc4150cf11c271267d8bc7979336edda8
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-03-07 02:00:51 +0800

    移除大写包

--- src/main/java/com/wintercogs/beyonddimensions/api/util/CapCtx.java
+++
@@ -1,9 +0,0 @@
-package com.wintercogs.beyonddimensions.api.util;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-
-public record CapCtx(Level level, BlockPos pos, BlockEntity be)
-{
-}
