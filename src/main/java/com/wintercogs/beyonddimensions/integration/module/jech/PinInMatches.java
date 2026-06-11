760c5bab610045b5d124fa8dbe0df1689363bf64
Frostbite-time <81908334+Frostbite-time@users.noreply.github.com>
2026-06-11 19:11:35 +0800

    恢复jech兼容

--- src/main/java/com/wintercogs/beyonddimensions/integration/module/jech/PinInMatches.java
+++ src/main/java/com/wintercogs/beyonddimensions/integration/module/jech/PinInMatches.java
@@ -1,11 +1,12 @@
 package com.wintercogs.beyonddimensions.integration.module.jech;
 
 
+import me.towdium.jecharacters.utils.Match;
+
 public class PinInMatches
 {
     public static boolean contains(String str, String other)
     {
-        //return Match.contains(str, other);
-        return false;
+        return Match.contains(str, other);
     }
 }
